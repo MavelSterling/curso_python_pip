097a6061f8237158204b4590fea62f4af2287f1d
MavelSterling <mavelynsl@gmail.com>
2023-02-25 19:23:04 -0500

    Update charts.py

--- app/charts.py
+++ app/charts.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 def generate_bar_chart(name, labels, values):
   fig, ax = plt.subplots()
   ax.bar(labels, values)
-  plt.savefig(f'./imgs/{name}.png')
+  #plt.savefig(f'./imgs/{name}.png')
+  plt.savefig('{}.png'.format(name))
   plt.close()
 
 def generate_pie_chart(labels, values):
